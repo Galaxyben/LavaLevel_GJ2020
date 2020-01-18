27636062e3db2a1ef464d40f3c4a36b8772c35e3
Ngakk <ngakwastaken@gmail.com>
2020-01-18 15:08:06 -0600

    Movent testing scene nad bumper

--- Assets/_main/Movement/Script/StandardBallMovement.cs
+++ Assets/_main/Movement/Script/StandardBallMovement.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class StandardBallMovement : BallMovement
 {
+    public Collider trigger;
+    public float bumperForce;
 
+    private List<Vector3> points = new List<Vector3>();
+
+    [Header("Debug")]
+    public Renderer rend;
+    protected override void Start()
+    {
+        base.Start();
+
+        trigger.enabled = false;
+    }
 
     public override void Special()
     {
-        
+        trigger.enabled = true;
+        rend.material.color = Color.red;
+    }
+
+    private void Update()
+    {
+        if(trigger.enabled && points.Count > 0)
+        {
+            //rigi.AddForce
+        }
+
+        trigger.enabled = false;
+        rend.material.color = Color.white;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        points.Add(other.ClosestPoint(transform.position));
     }
 }
