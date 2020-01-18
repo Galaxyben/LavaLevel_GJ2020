ef4822ecccf8fe0c14197022822460a550ddf3e7
Ngakk <ngakwastaken@gmail.com>
2020-01-18 03:30:19 -0600

    Damage testing

---
+++ Assets/_main/Movement/Script/BallScaler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallScaler : MonoBehaviour
+{
+    Rigidbody rigi;
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    void GetHit()
+    {
+        transform.localScale = transform.localScale + Vector3.one * 0.1f;
+        rigi.mass += 0.5f;
+    }
+}
